9622d6a5fea4d28f2a8d619cc0e966bb0d67c158
Reisa Maulidya Rohman <145237051+reeisa@users.noreply.github.com>
2023-10-04 20:23:30 +0800

    Add files via upload

---
+++ posttest3/javascript.js
@@ -0,0 +1,60 @@
+// alert button
+const alertButton = document.getElementById("alert");
+alertButton.addEventListener("click", function() {
+    alert("Ticket is not available for purchase yet.");
+});
+
+// toggle light&dark mode
+const checkbox = document.getElementById("checkbox");
+checkbox.addEventListener("change", function() {
+    if (checkbox.checked) {
+        document.body.classList.add("light");
+    } else {
+        document.body.classList.remove("light");
+    }
+});
+
+// countdown
+function updateCountdown() {
+    var compareDate = new Date();
+    compareDate.setDate(compareDate.getDate() + 30);
+
+    var timer = setInterval(function () {
+        var now = new Date();
+        var difference = compareDate - now;
+
+        if (difference <= 0) {
+            clearInterval(timer);
+            difference = 0;
+        }
+
+        var seconds = Math.floor((difference / 1000) % 60);
+        var minutes = Math.floor((difference / 1000 / 60) % 60);
+        var hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+        document.getElementById("days").textContent = days;
+        document.getElementById("hours").textContent = hours;
+        document.getElementById("minutes").textContent = minutes;
+        document.getElementById("seconds").textContent = seconds;
+    }, 1000);
+}
+updateCountdown();
+
+// smooth scroll
+function smoothScrollingTo(target) {
+    $('html, body').animate({ scrollTop: $(target).offset().top }, 500);
+  }
+
+  $('a[href^="#"]').on('click', function (event) {
+    event.preventDefault();
+    smoothScrollingTo(this.hash);
+  });
+
+  $(document).ready(function () {
+    smoothScrollingTo(location.hash);
+  });
+
+
+
+
